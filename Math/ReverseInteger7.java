df84391b714c8096d7fe5f43764549a3fea9d0cf
Akif <akiif.dev@gmail.com>
2022-08-13 20:37:36 +0530

    Added the ReverseInteger7.java file

---
+++ Math/ReverseInteger7.java
@@ -0,0 +1,16 @@
+package Math;
+
+public class ReverseInteger7 {
+    public int reverse(int x) {
+        long rev = 0;
+        while (x!=0) {
+            rev = rev * 10 + x % 10;
+            x = x/10;
+        }
+
+        if (rev >= Integer.MIN_VALUE && rev <= Integer.MAX_VALUE)
+            return (int) rev;
+        else
+            return 0;
+    }
+}
