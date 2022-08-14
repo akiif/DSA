b50087823a29a47a0dc778ba58c540e45644b830
Akif <akiif.dev@gmail.com>
2022-08-14 23:03:42 +0530

    Added the TwoSum1.java file

---
+++ Arrays/TwoSum1.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+//https://leetcode.com/problems/two-sum/
+
+public class TwoSum1 {
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int res[] = {i, j};
+                    return res;
+                }
+            }
+        }
+        return null;
+    }
+}
