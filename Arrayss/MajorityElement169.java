288ecd1fab060a7adb4cad1288a5189fa46bfbfe
Akif Mohammed <akiif.dev@gmail.com>
2022-08-17 00:02:40 +0530

    Added the MajorityElement169.java file.

--- Arrayss/MajorityElement169.java
+++ Arrayss/MajorityElement169.java
@@ -2,6 +2,8 @@ package Arrayss;
 
 import java.util.Arrays;
 
+//https://leetcode.com/problems/majority-element/
+
 public class MajorityElement169 {
     public int majorityElement(int[] nums) {
         Arrays.sort(nums);
