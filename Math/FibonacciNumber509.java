a28ac68e56739b83e851ac4112647c6e075de4dd
Akif Mohammed <akiif.dev@gmail.com>
2022-08-20 18:50:41 +0530

    Added the FibonacciNumber509.java files.

---
+++ Math/FibonacciNumber509.java
@@ -0,0 +1,16 @@
+package Math;
+
+//https://leetcode.com/problems/fibonacci-number/
+
+public class FibonacciNumber509 {
+    public int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        } else {
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
