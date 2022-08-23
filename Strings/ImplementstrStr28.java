9f492d9532e85aa37eed60ef21e0db92e2e66c87
Akif Mohammed <akiif.dev@gmail.com>
2022-08-23 17:46:24 +0530

    Added the ImplementstrStr28.java file.

---
+++ Strings/ImplementstrStr28.java
@@ -0,0 +1,23 @@
+package Strings;
+
+//https://leetcode.com/problems/implement-strstr/
+
+public class ImplementstrStr28 {
+
+
+    public int strStr(String haystack, String needle) {
+        int index = -1;
+        int n =  haystack.length();
+        String str = "";
+        for (int i = 0; i <= n - 1; i++) {
+            for (int j = n; j >= i; j--) {
+                str = haystack.substring(i, j);
+                if (str.equals(needle)) {
+                    index = i;
+                    return index;
+                }
+            }
+        }
+        return index;
+    }
+}
