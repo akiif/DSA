8a1f53e2f8748449c7d726c57d1376ada9fbb4f1
Akif Mohammed <akiif.dev@gmail.com>
2022-08-28 21:47:10 +0530

    Updated the NumberofGoodPairs1512.java file.

--- Arrayss/NumberofGoodPairs1512.java
+++ Arrayss/NumberofGoodPairs1512.java
@@ -4,7 +4,20 @@ package Arrayss;
 
 public class NumberofGoodPairs1512 {
 
-    public int numIdenticalPairs(int[] nums) {
+    public int numIdenticalPairs1(int[] nums) {
+        int[] arr = new int[101];
+        for(int i = 0;i<nums.length;i++){
+            arr[nums[i]] ++;
+        }
+        int ans = 0;
+        for(int i = 0;i<arr.length;i++){
+            int count = arr[i];
+            ans += count*(count - 1)/2;
+        }
+        return ans;
+    }
+
+    public int numIdenticalPairs2(int[] nums) {
         int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
