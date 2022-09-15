de586b75b050d2f610add13988c7477702d5bf2f
Akif Mohammed <akiif.dev@gmail.com>
2022-09-15 19:50:11 +0530

    Added the FibonacciFormula.java file.

--- TimeAndSpaceComplexity/FibonacciFormula.java
+++ TimeAndSpaceComplexity/FibonacciFormula.java
@@ -3,16 +3,15 @@ package TimeAndSpaceComplexity;
 public class FibonacciFormula {
 
     public static void main(String[] args) {
-        int num = 2;
+        int num = 50;
         System.out.println(Fibonacci(num));
     }
 
-    static int Fibonacci(int num) {
-        double rootFive = 1;
-        rootFive = Math.pow(rootFive, 0.2);
+    static long Fibonacci(int num) {
+        double rootFive = Math.sqrt(5);
         System.out.println(rootFive);
         double fib = (((1 / rootFive) * (Math.pow( (1 + rootFive) / 2, num)))
                     - ((1 / rootFive) * (Math.pow( (1 - rootFive) / 2, num))));
-        return (int) fib;
+        return (long) fib;
     }
 }
