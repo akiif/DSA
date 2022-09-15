ae2fce6a9f1ecb29b78c2b0003d18782f1f89d65
Akif Mohammed <akiif.dev@gmail.com>
2022-09-15 20:00:17 +0530

    Removed the "Time and Space Complexity" folder and updated the FibonacciFormula.java file.

--- TimeAndSpaceComplexity/FibonacciFormula.java
+++ TimeAndSpaceComplexity/FibonacciFormula.java
@@ -3,15 +3,14 @@ package TimeAndSpaceComplexity;
 public class FibonacciFormula {
 
     public static void main(String[] args) {
-        int num = 50;
+        int num = 6;
         System.out.println(Fibonacci(num));
     }
 
     static long Fibonacci(int num) {
         double rootFive = Math.sqrt(5);
-        System.out.println(rootFive);
         double fib = (((1 / rootFive) * (Math.pow( (1 + rootFive) / 2, num)))
                     - ((1 / rootFive) * (Math.pow( (1 - rootFive) / 2, num))));
-        return (long) fib;
+        return Math.round(fib);
     }
 }
